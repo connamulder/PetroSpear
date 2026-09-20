1b465f97ed2bf363c4c5dac9fcd2cc294f90f425
Chen Zhongliang <59856652+connamulder@users.noreply.github.com>
2026-09-20 14:02:01 +0800

    Update cnn_train_multi_loss_rank.py

--- cnn_train_multi_loss_rank.py
+++ cnn_train_multi_loss_rank.py
@@ -1,6 +1,6 @@
 """
     @Project: PetroSpear
-    @File   : 06_cnn_train_multi_loss.py
+    @File   : cnn_train_multi_loss.py
     @Author : mulder
     @E-mail : c_mulder@163.com
     @Date   : 2026-07-01
